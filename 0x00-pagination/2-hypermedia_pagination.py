0453ee4bf1ae1b76d05bd6543a2d9a4fa5e2c7db
Teepee78 <eniolatoluwalase78@gmail.com>
2023-02-20 12:24:29 +0100

    Update

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -119,14 +119,3 @@ class Server:
             "prev_page": prev_page,
             "total_pages": total_pages
         }
-
-
-server = Server()
-
-print(server.get_hyper(1, 2))
-print("---")
-print(server.get_hyper(2, 2))
-print("---")
-print(server.get_hyper(100, 3))
-print("---")
-print(server.get_hyper(3000, 100))
